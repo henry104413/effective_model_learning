e1027367660bc53d1b0677b0d53088e3726491ce
henry104413 <jindrich0hajek@gmail.com>
2024-09-25 02:28:44 +0100

    output generation in separate module now

--- execute.py
+++ execute.py
@@ -11,15 +11,11 @@ from learning_model import LearningModel
 
 from learning_chain import LearningChain
 
-from definitions import Constants
+from output import Output
 
 import numpy as np
 
-import matplotlib.pyplot as plt
-
 import time
-
-import pickle
 
 
 
@@ -118,7 +114,7 @@ initial_guess.build_operators()
 # instance of learning (quest for best model):
 quest = LearningChain(target_times = ts, target_data = measurements, initial_guess = initial_guess)
 
-costs = quest.learn(100000)
+costs = quest.learn(50)
 
 best = quest.best
 
@@ -132,14 +128,11 @@ best_data = best.calculate_dynamics(ts)
     
 # controls bundle:
     
-class Save():    
-    comparison = True
-    cost = True
-    GT_model = True
-    best_model = True
-    GT_text = True
-    best_text = True
-
+class Toggles():    
+    comparison = True # plot comparison of dynamics
+    cost = True # plot cost function progression
+    pickle = True # save selected models as pickles
+    text = True # save selected models as text
 
 
 # unique name (date and time stamp):
@@ -147,66 +140,16 @@ class Save():
 timestamp = time.strftime("%Y_%m_%d_%H%M%S", time.gmtime())
 
 
+# create outputs:
 
-# plot dynamics comparison:
-
-plt.figure()
-plt.plot(ts*Constants.t_to_sec, measurements, 'r-', label = 'ground truth')
-plt.plot(ts*Constants.t_to_sec, best_data, 'b--', label = 'learned model')
-plt.xlabel('time (fs)')
-plt.ylabel('qubit excited population')
-plt.ylim([0,1.1])
-plt.legend()
-if Save.comparison: plt.savefig(timestamp + '_comparison.svg')
-
-
-
-# plot cost function progression:
-    
-plt.figure()
-plt.plot(costs, 'm-', linewidth = 0.1, markersize = 0.1)
-plt.yscale('log')
-plt.xlabel('iteration')
-plt.ylabel('cost')
-if Save.cost: plt.savefig(timestamp + '_cost.svg')
-
-
-
-# save ground truth instance:
-
-if Save.GT_model:
-    with open(timestamp + '_GT.pickle', 'wb') as filestream:
-        pickle.dump(GT,  filestream)
-    
-    
-    
-# save best model instance:
-
-if Save.best_model:
-    with open(timestamp + '_best.pickle', 'wb') as filestream:
-        pickle.dump(best,  filestream)
-    
-    
-    
-# save ground truth text descritpion:
-
-if Save.GT_text:
-    with open(timestamp + '_GT.txt', 'w') as filestream:
-        filestream.write(GT.description())
+Output(toggles = Toggles, filename = timestamp,
+       dynamics_ts = ts, dynamics_datasets = [measurements, best_data], dynamics_labels = ['GT', 'learned'],
+       cost = costs,
+       models_to_save = [GT, best],
+       model_names = ['GT', 'best']
+       )
     
 
-
-# save ground truth text descritpion:
-
-if Save.GT_text:
-    with open(timestamp + '_best.txt', 'w') as filestream:
-        filestream.write(best.description())
-    
-
-
-    
-    
-    
 #%% works!
 # add accompanying document with quick description or something
 # could be jason or really a txt will do
--- output.py
+++ output.py
@@ -3,18 +3,91 @@
 """
 Effective model learning
 @author: Henry (henry104413)
+
 """
 
+import pickle
+
+import matplotlib.pyplot as plt
+
+from definitions import Constants
+
+# import numpy as np
+
+
+
 class Output():
-
-
-    def __init__(self, fontsize = False):
-        
-        
-        self.fontsize = False
-        
-        pass
     
-    # plot comparison models:
+    # to do: set font here for all plots
+
+
+    def __init__(self, *, toggles, filename,
+                 dynamics_ts = False, dynamics_datasets = [], dynamics_labels = [],
+                 cost = [],
+                 models_to_save = [],
+                 model_names = [],
+                 fontsize = False):
         
         
+        
+        self.fontsize = fontsize
+        
+        
+    
+        # plot dynamics comparison (up to 4):
+            
+        if toggles.comparison:
+            
+            colours = ['r-', 'b--', 'k:', 'g-.']
+            
+            # ensure label selector doesn't go out of bounds
+            def get_label(i):
+                if not dynamics_labels or len(dynamics_labels) < len(dynamics_datasets): return None
+                else: return dynamics_labels[i]
+            
+            plt.figure()
+            
+            for i in range(min(len(dynamics_datasets), 4)):    
+                plt.plot(dynamics_ts*Constants.t_to_sec, dynamics_datasets[i], colours[i], label = get_label(i))
+                plt.xlabel('time (fs)')
+                plt.ylabel('qubit excited population')
+                plt.ylim([0,1.1])
+                plt.legend()
+                plt.savefig(filename + '_comparison.svg')
+    
+    
+    
+        # plot cost function progression:
+        
+        if toggles.cost:     
+        
+            plt.figure()
+            plt.plot(cost, 'm-', linewidth = 0.1, markersize = 0.1)
+            plt.yscale('log')
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.savefig(filename + '_cost.svg')
+    
+    
+        
+        # save model instances (as text and/or pickle as specified):
+            
+        # ensure name selector doesn't go out of bounds:
+        def get_model_name(i):
+            if not model_names or len(model_names) < len(models_to_save): return '_' + str(i)
+            else: return model_names[i]
+            
+        for i, model in enumerate(models_to_save):
+            
+            # as pickle:
+        
+            if toggles.pickle:
+                with open(filename + get_model_name(i) +'.pickle', 'wb') as filestream:
+                    pickle.dump(models_to_save[i],  filestream)
+                
+            # as text
+            
+            if toggles.text:
+                with open(filename + get_model_name(i) + '.txt', 'w') as filestream:
+                    filestream.write(models_to_save[i].description())
+         
