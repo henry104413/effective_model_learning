18cd6e7756c6affd83f8abb1d0ee3f73762ba6da
henry104413 <jindrich0hajek@gmail.com>
2025-01-21 17:07:26 +0000

    process handling deprecated method for joint addition and removal of L deleted

--- process_handling.py
+++ process_handling.py
@@ -12,12 +12,12 @@ from model import Model
 from learning_model import LearningModel
 
 
+
 # executes any modification to model processes (operators)
 # stores available process library
 
 # methods should take model as argument and work on it
 # same process handling object expected to be used on various models 
-
 
 class ProcessHandler():
     
@@ -32,93 +32,6 @@ class ProcessHandler():
 
         
 
-    # decides and carries out either addition or removal of a random L on a random defect
-    # - currently deprecated
-    
-    def add_toss_L(self, model = False, Ls_library = False):
-        
-            
-        
-        # set model and process library if passed now, then check both set to appropriate types:
-        
-        if isinstance(model, LearningModel) or isinstance(model, Model):
-            
-            self.model = model
-            
-        if isinstance(Ls_library, dict):
-            
-            self.Ls_library = Ls_library
-            
-        if not (isinstance(self.Ls_library, dict)):
-            
-            raise SystemError('Model modification failed due to process library missing')
-            
-        if not (isinstance(model, LearningModel) or isinstance(model, Model)):
-        
-            raise SystemError('Model modification failed due to model not specified')
-            
-            
-        
-        # decide which system and whether to add or remove
-        # note: for now acts on defects' Ls only
-        
-        subsystems = [x for x in self.model.TLSs if not x.is_qubit]
-        
-        subsystem = np.random.choice(subsystems)
-        
-        adding_of_L_probability = 0.5 # probability this step adds new process as opposed to removing
-        
-        if np.random.uniform() < adding_of_L_probability: # ie, add new process
-        
-            # randomly choose one process library operator not yet acting on chosen subsystem
-        
-            existing = [x for x in subsystem.Ls]
-        
-            options = [x for x in self.Ls_library if x not in existing]
-                
-            if options:
-            
-                op = np.random.choice(options)
-                
-                subsystem.Ls[op] = self.Ls_library[op]
-                
-                # note: this directly modifies variable another classe's object
-                # ...perhaps could be changed to instead work via method of that class?
-                
-                self.model.build_operators()
-                
-                print('adding operator ' + op + ' to TLS no. ' + str(self.model.TLSs.index(subsystem)))
-                
-            else:
-                
-                print('no options for adding operator to TLS no. ' + str(self.model.TLSs.index(subsystem)))
-            
-        else: # ie, remove existing process
-        
-            options = [x for x in subsystem.Ls]
-            
-            if options:
-                
-                op = np.random.choice(options)
-            
-                subsystem.Ls.pop(op)
-                
-                print('removing operator ' + op + ' from TLS no. ' + str(self.model.TLSs.index(subsystem)))
-                
-            else:
-                
-                print('no options for removing operator from TLS no. ' + str(self.model.TLSs.index(subsystem)))
-            
-            
-            
-        # ADD check if process already present -- then choose another -- check also if options exhausted
-        
-        # or why not just choose difference... 
-        
-        return None
-            
-        
-            
     # adds random new single-site Linblad process from process library to random subsystem (qubit or defect for now) 
     # with rate drawn from uniform distribution between bounds given by tuple for each process library entry  
     # works on and returns argument model
@@ -183,10 +96,12 @@ class ProcessHandler():
             pass
         
         
+        
     # sets process library post-constructor: 
     def define_Ls_library(self, Ls_library):
     
         self.Ls_library = Ls_library
+        
         
         
     # resets Ls library to initial one:    
