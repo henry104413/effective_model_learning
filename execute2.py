4ec59e583e055606ee49bfd574c107969796af92
henry104413 <jindrich0hajek@gmail.com>
2025-04-07 15:12:00 +0100

    graph being generated with all edges and labels added to coupling edges too, still need to fix lindblad labels, but mainly no plotting methods work for now

--- execute2.py
+++ execute2.py
@@ -73,7 +73,7 @@ quest = learning_chain.LearningChain(target_times = ts,
                       initial = (1, 2), # (qubit energy, number of defects)
                       qubit_initial_state = qubit_initial_state,
                       
-                      max_chain_steps = 2000,
+                      max_chain_steps = 100,
                       chain_step_options = {
                           'tweak all parameters': 0.3,
                           'add L': 0.05,
@@ -163,7 +163,7 @@ timestamp = time.strftime("%Y_%m_%d_%H%M%S", time.gmtime())
 # maybe add additional check to in unlikely case parallel run ends up with same name...
 
 # create outputs:
-output.Output(toggles = Toggles, filename = timestamp,
+O = output.Output(toggles = Toggles, filename = timestamp,
        dynamics_ts = [ts, evaluation_ts],
        dynamics_datasets = [measurement_datasets, best_datasets],
        dynamics_datasets_labels = ['measured', 'learned'],
@@ -178,7 +178,59 @@ output.Output(toggles = Toggles, filename = timestamp,
 
 
 #%% 
+raise SystemExit()
 
 
+import itertools as it
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
+def draw_labeled_multigraph(G, attr_name, ax=None):
+    """
+    Length of connectionstyle must be at least that of a maximum number of edges
+    between pair of nodes. This number is maximum one-sided connections
+    for directed graph and maximum total connections for undirected graph.
+    """
+    # Works with arc3 and angle3 connectionstyles
+    connectionstyle = [f"arc3,rad={r}" for r in it.accumulate([0.15] * 4)]
+    # connectionstyle = [f"angle3,angleA={r}" for r in it.accumulate([30] * 4)]
+
+    pos = nx.shell_layout(G)
+    nx.draw_networkx_nodes(G, pos, ax=ax)
+    nx.draw_networkx_labels(G, pos, font_size=20, ax=ax)
+    nx.draw_networkx_edges(
+        G, pos, edge_color="grey", connectionstyle=connectionstyle, ax=ax
+    )
+
+    labels = {
+        tuple(edge): f"{attr_name}={attrs[attr_name]}"
+        for *edge, attrs in G.edges(keys=True, data=True)
+    }
+    nx.draw_networkx_edge_labels(
+        G,
+        pos,
+        labels,
+        connectionstyle=connectionstyle,
+        label_pos=0.3,
+        font_color="blue",
+        bbox={"alpha": 0},
+        ax=ax,
+    )
+
+
+nodes = "ABC"
+prod = list(it.product(nodes, repeat=2))
+pair_dict = {f"Product x {i}": prod * i for i in range(1, 5)}
+
+
+fig, axes = plt.subplots(2, 2)
+for (name, pairs), ax in zip(pair_dict.items(), np.ravel(axes)):
+    G = nx.MultiDiGraph()
+    for i, (u, v) in enumerate(pairs):
+        G.add_edge(u, v, w=round(i / 3, 2))
+    draw_labeled_multigraph(G, "w", ax)
+    ax.set_title(name)
+fig.tight_layout()
+plt.show()
