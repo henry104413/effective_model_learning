eee6824b47552d1688505c4cd7e2f5d28ee1a3cf
henry104413 <jindrich0hajek@gmail.com>
2025-04-25 16:25:28 +0100

    completed sorting by clusters of model files

---
+++ launch_execute_learning.sh
@@ -0,0 +1,58 @@
+# Effective model learning - bash launcher
+# @author: Henry (henry104413)
+
+# bash to carry out learning
+# general run parameters set here
+# advanced learning hyperparameters set in execute.py file
+# code files musty be in the same directory 
+
+
+# set below:
+# 1) target csv file where pairs of columns are individual datasets,
+# - any annotations will be skipped
+# 2) experiment name to use in output filenames
+# 3) array of different numbers of defects to run with
+# 4) array of repetitions numbers for each defect number
+# 5) array of iterations (proposals) numbers for each defect numbers
+# note:
+# 4) and 5) either have to be same length as 3),
+# or length 1 if same settings to be used for each defect number
+# but in each case must be arrays!  
+target_csv="Witnessing_Fig4b.csv"
+experiment_name="splitting_test_Wit_Fig4b-grey"
+defects_numbers=(1)
+repetitions_numbers=(10)
+iterations_numbers=(2000)
+
+
+# execution:
+for i in ${!defects_numbers[@]}; do
+    defects_number=${defects_numbers[i]}
+
+    # determine iterations number for this number of defects:
+    if [ ${#iterations_numbers[@]} -eq ${#defects_numbers[@]} ]; then
+    	iterations_number=${iterations_numbers[i]}
+    elif [ ${#iterations_numbers[@]} -eq 1 ]; then
+    	iterations_number=${iterations_numbers[0]}
+    else
+    	# if not specified properly, will pass 0
+    	# then execute file will use its default value
+    	iterations_number=0	
+    fi
+    
+    # determine repetitions number for this number of defects:
+    if [ ${#repetitions_numbers[@]} -eq ${#defects_numbers[@]} ]; then
+    	repetitions_number=${repetitions_numbers[i]}
+    elif [ ${#repetitions_numbers[@]} -eq 1 ]; then
+    	repetitions_number=${repetitions_numbers[0]}
+    else
+    	# if not specified properly, set default here:
+    	repetitions_number=1	
+    fi
+    
+    for ((rep=1; rep<=repetitions_number; rep++)); do
+	echo launching for $defects_number defects repetition no. $rep
+	nohup python execute.py "$target_csv" "$experiment_name" "$defects_number" "$rep" "$iterations_number"  </dev/null &>"$experiment_name"_D"$defects_number"_R"$rep"_prog.txt &
+	done
+done
+
---
+++ launch_sort_by_clusters.sh
@@ -0,0 +1,50 @@
+# Effective model learning - bash launcher
+# @author: Henry (henry104413)
+
+# bash to sort model files into separate folders given clusters count
+# according to existing cluster assignment dictionary 
+
+# model files must be in this folder and follow naming convention:
+# <experiment name>_D<number of defects>_R<run number>_<which output>.<type>
+# alongside the assignment lists produced by sort_by_clusters.py
+# <experiment name>_D<number of defects>_Cs<clusters count>_C<cluster number>.txt
+
+# for now to be called separately for each defects number
+# later will allow array specification
+
+
+
+experiment_name="splitting_test_Wit_Fig4b-grey"
+defects_number=1
+clusters_count=3
+
+
+python sort_by_clusters.py "$experiment_name" "$defects_number" "$clusters_count" 
+
+assignment_name="$experiment_name"_D"$defects_number"_Cs"$clusters_count"
+
+mkdir "$assignment_name"
+
+for ((cluster=0; cluster<clusters_count; cluster++)); do
+
+    # make directory for each cluster; in it goes champion and subdirectory with all cluster members
+    list="$assignment_name"_C"$cluster".txt
+    mkdir "$assignment_name"/"$assignment_name"_C"$cluster"
+    mkdir "$assignment_name"/"$assignment_name"_C"$cluster"/"$assignment_name"_C"$cluster"_all
+    
+    # copy model files from current to corresponding directories:
+    i=1
+    while IFS= read -r line; do
+        if [ $i -eq 1 ]; then
+            cp $line* "$assignment_name"/"$assignment_name"_C"$cluster"
+            echo first: copying "$line"
+        else
+            cp $line* "$assignment_name"/"$assignment_name"_C"$cluster"/"$assignment_name"_C"$cluster"_all
+            echo others: copying "$line"
+        fi 
+        ((++i))
+    done < "$assignment_name"_C"$cluster".txt
+done    
+    
+    
+    
