249238524736837464f7d9ee0e42d214c1d08c7f
henry104413 <jindrich0hajek@gmail.com>
2026-07-22 14:53:48 +0100

    integrator_gradient now working to optimise ODE parameters based on likelihood gradient using - forcing single JIT compilation avoids memory errors and massively speeds up runtime

--- integrator_gradient.py
+++ integrator_gradient.py
@@ -29,31 +29,40 @@ import scipy as sp # nope! not with jax
 import numpy as np
 import matplotlib.pyplot as plt
 import diffrax
+import equinox as eqx # this is for debugging and checking compilation...
 
-params = (float(2),float(3), float(2.5)) # must be a tuple of floats!!! formerly (a,b,c)
+params = jax.numpy.array([2.1,8,4,1], dtype=float) # must be a tuple of floats!!! formerly (a,b,c)
+#guess_params = jax.numpy.array([2.5 for i in params], dtype=float)
+guess_params = jax.numpy.array([5,10,4.1,1.3], dtype=float)
 # this seriously must be floats!! jax hates int... even with explicit argument to allow int it breaks
-dim = len(params)
-y0 = float(0)
-ts = np.linspace(0, 3, 100)
-variance = float(0.1)
+y0 = jax.numpy.array(0, dtype=float)
+ts = jax.numpy.linspace(0, 3, 1000, dtype=float)
+variance = jax.numpy.array(1, dtype=float)
 
-# differential equation term:
-def H(t,y,args): 
-    # two things!! 
-    # scipy by default assumes arguments (y,t, *args), whereas diffrax (t,y,args) - so:
-    # so 1) passing ((a,b,c),) - this receives already unwrapped whatever was passed
-    # hence assuming args is a TUPLE now
-    # and 2) need to set tfirst = True in scipy integrator
+# # differential equation term:
+# def H(t,y,args): 
+#     # two things!! 
+#     # scipy by default assumes arguments (y,t, *args), whereas diffrax (t,y,args) - so:
+#     # so 1) passing ((a,b,c),) - this receives already unwrapped whatever was passed
+#     # hence assuming args is a TUPLE now
+#     # and 2) need to set tfirst = True in scipy integrator
     
-    #a,b,c = args
-    #return a - b*t - c*y
+#     #a,b,c = args
+#     #return a - b*t - c*y
     
-    return args[0] - args[1]*t - args[2]*y
+#     return args[0] - args[1]*t - args[2]*y
 
+dy_dt = lambda t,y,args: args[0] - args[1]*t - args[2]*y + args[3]*y*t
+
+switch_print_iterations = False
+switch_save_likelihood = False
 
 # scipy solution - target data:
-ys_scipy = sp.integrate.odeint(H, y0, ts, args=(params,), tfirst=True)
+ys_scipy = sp.integrate.odeint(dy_dt, y0, ts, args=(params,), tfirst=True)
 D = jax.numpy.reshape(jax.numpy.array(ys_scipy), shape = (len(ys_scipy)))
+
+
+
 
 
 # take care!!! shapes of arrays from scipy and diffrax are different!!!
@@ -81,9 +90,14 @@ solver = diffrax.Kvaerno5()
 # ram taken (by "python") grows with every iteration... it shouldn't... not explained by what's stored
 # so it keeps the compiled functions for each iteration???
 # either dump it or better still stop it being recompliled every time??? ok at 154 5.4GB RAM crashed
+# still - at 154
+
+# ok... Patrick says recomplilation might be caused by feeding python types into JITed functions... explicitly only feed jax arrays!!!
 
 
 # find gradient of likelihood now as function of guessed a,b,c
+@jax.jit # this causes it to not be recompiled!!!
+@eqx.debug.assert_max_traces(max_traces=1)
 def FL(guess_params,D,variance,ts,y0):
     
     # # differential equation term:
@@ -102,7 +116,7 @@ def FL(guess_params,D,variance,ts,y0):
     #lambda t,y,args:  
     
     # diffrax (using jax) solution:
-    sol_diffrax = diffrax.diffeqsolve(terms = diffrax.ODETerm(lambda t,y,args: args[0] - args[1]*t - args[2]*y),
+    sol_diffrax = diffrax.diffeqsolve(terms = diffrax.ODETerm(dy_dt),
                                      solver = solver,
                                      t0 = ts[0], t1 = ts[-1],
                                      dt0 = (ts[1] - ts[0])/100,
@@ -110,7 +124,7 @@ def FL(guess_params,D,variance,ts,y0):
                                      saveat = diffrax.SaveAt(ts = ts), # steps=True),
                                      args = guess_params,
                                      stepsize_controller = stepsize_controller,
-                                     max_steps=int(1e6)
+                                     max_steps=int(1e8)
                                      )
     
     ys = sol_diffrax.ys
@@ -142,7 +156,6 @@ y0 = jax.numpy.array(y0)
 ts = jax.numpy.array(ts)
 
 #guess_params = jax.numpy.array(tuple(i+0.5 for i in params)) # this is immutable so can save without deepcopy (tested)
-guess_params = jax.numpy.array((float(2.5), float(2.5), float(2.5)))
 guess_params_init = guess_params
 likelihood_init = FL(guess_params, D,variance,ts,y0)
 print('starting params:\n' + str(guess_params_init))
@@ -162,11 +175,11 @@ grad_FL = jax.grad(FL, argnums = 0, allow_int=True)
 
 
 # optimise parameters:
-step_size_mean_std = (0.001, 0.0002) #jax.numpy.array(0.1)
-grad_scale = 100
-grad_min, grad_max = -float(grad_scale), float(grad_scale)
+step_size_mean_std = jax.numpy.array([0.0002, 0.00002], dtype = float) #jax.numpy.array(0.1)
+grad_max_val = jax.numpy.array(200, dtype = float)
+
 # note: currently same for all parameters
-max_steps = int(200)
+max_steps = int(10)
 explored_params = []
 explored_likelihood = []
 explored_params.append(guess_params)
@@ -176,7 +189,7 @@ for i in range(max_steps):
     
     # find gradient at current parameters AND CLIP
     gradient = grad_FL(guess_params, D, variance, ts, y0)
-    clipped_gradient = jax.numpy.clip(gradient, min = grad_min, max = grad_max)
+    clipped_gradient = jax.numpy.clip(gradient, min = -grad_max_val, max = grad_max_val)
     
     # sample step size AND CONVERT TO JAX
     step_size = np.random.normal(*step_size_mean_std, size = clipped_gradient.shape)
@@ -192,24 +205,32 @@ for i in range(max_steps):
     # update with step of fixed size times gradient - unstable as hell (sometimes gradient is very steep!)
     # guess_params += step_size*jax.numpy.sign(gradient)
     
-    likelihood_current = FL(guess_params, D,variance,ts,y0)
+    if switch_save_likelihood:
+        likelihood_current = FL(guess_params, D,variance,ts,y0)
+        explored_likelihood.append(likelihood_current)
     # print for troubleshooting:
-    print('iteration: ' + str(i))    
-    print('step sizes: ' + str(step_size))
-    print('gradient: ' + str(gradient))
-    print('clipped gradient: ' + str(clipped_gradient))
-    print('new params: ' + str(guess_params))
-    print('current likelihood: ' + str(likelihood_current))
+    if switch_print_iterations:
+        print('iteration: ' + str(i))    
+        print('step sizes: ' + str(step_size))
+        print('gradient: ' + str(gradient))
+        print('clipped gradient: ' + str(clipped_gradient))
+        print('new params: ' + str(guess_params))
+        if switch_save_likelihood:
+            print('current likelihood: ' + str(likelihood_current))
     
     explored_params.append(guess_params)
-    explored_likelihood.append(likelihood_current)
+    
     
 print('final params:\n' + str(guess_params))
-print('final log likelihood:\n' + str(likelihood_current))
+if switch_save_likelihood:
+    print('final log likelihood:\n' + str(likelihood_current))
+else:
+    print(FL(guess_params, D,variance,ts,y0))
 
 #%%  
 mu, sigma = step_size_mean_std
-experiment = r'grad $\times \alpha$' + '\n' + r'$\mu=0.01, \sigma=0.0$'
+experiment = 'grad_max=' + str(grad_max_val) + '_alpha_N(' + str(mu) + ',' + str(sigma) + ')'
+title = r'grad $\in [$' + str(-grad_max_val) + ',' + str(grad_max_val) + ']' + '\n' + r'$\alpha \sim N(\mu=$' + '{0:.2f}'.format(mu) + r'$, \sigma=$' + '{0:.2f}'.format(sigma) + ')' 
 plt.figure()
 params_progression = [[] for i in guess_params]
 colours = ['r','g','b','m']
@@ -219,22 +240,29 @@ for i in range(len(params)):
     plt.plot([params[i] for x in range(len(params_progression[i]))], ':', c = colours[i], label = 'true', alpha = 0.7)
 plt.ylabel('parameter')
 plt.xlabel('step')
-plt.title(experiment)
+plt.title(title)
 plt.savefig(experiment + 'params.svg', dpi = 1000, bbox_inches='tight')
 plt.figure()
 plt.plot(explored_likelihood)#, yscale='log')
+plt.yscale('symlog')
 plt.ylabel('log likelihood')
 plt.xlabel('step')
-plt.title(experiment)
+plt.title(title)
 plt.savefig(experiment + 'LL.svg', dpi = 1000, bbox_inches='tight')
-
-# grad_FL_wrt_a = jax.grad(FL, argnums = (0), allow_int=True)
-# print(grad_FL_wrt_a(guess_a,guess_b,guess_c,D,variance,ts,y0))
-# evaluated at arguments of function to be differentiated
-
-# CONTINUE HERE
-# TRIPLE CHECK THAT GRADIENT APPLIED WITH CORRECT SIGN!!!
-# # in both cases now likelihood epxplodes downwards...
-
-
-## mm the parameters dont start where they should - 
+plt.figure()
+plt.plot(ts, D, 'b-', label='scipy, OG', alpha=0.5)
+plt.title('integrated functions')
+plt.xlabel('t')
+plt.ylabel('y')
+sol_diffrax = diffrax.diffeqsolve(terms = diffrax.ODETerm(dy_dt),
+                                 solver = solver,
+                                 t0 = ts[0], t1 = ts[-1],
+                                 dt0 = (ts[1] - ts[0])/100,
+                                 y0 = y0,
+                                 saveat = diffrax.SaveAt(ts = ts), # steps=True),
+                                 args = guess_params,
+                                 stepsize_controller = stepsize_controller,
+                                 max_steps=int(1e6)
+                                 )
+plt.plot(ts,sol_diffrax.ys, 'r:', label='diffrax, learned')
+plt.legend()
